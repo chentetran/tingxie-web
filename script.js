0ab349ab9bd6df11e6bf4e2340290e84b74f504e
Vincent Tran <vincenttran23@gmail.com>
2016-02-23 22:09:01 -0500

    added style

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-document.getElementById('file').onchange = function(){
-
-  			var file = this.files[0];
-
-  			var reader = new FileReader();
-  			reader.onload = function(progressEvent){
-    			// Entire file
-			    console.log(this.result);
-
-			    // // By lines
-			    // var lines = this.result.split('\n');
-			    // for(var line = 0; line < lines.length; line++){
-			    //   console.log(lines[line]);
-			    // }
-  			};
-  			reader.readAsText(file);
-		};
+var File = new XMLHttpRequest();
+File.open("GET", file, false);
+File.onreadystatechange = function() {
+	if(File.readyState ==)
+}
